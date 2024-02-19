86cdad01cef2d5e884c313efb5c67ea0486d8e53
complex65 <154450317+complex65@users.noreply.github.com>
2024-02-19 18:14:22 +1100

    Add files via upload

---
+++ How_old_are_you.py
@@ -0,0 +1,8 @@
+age = int(input("what year where you born: "))
+current = 2024
+var = 1950
+while age < 1950:
+    print('Enter your real birthday')
+    age = int(input("what year where you born: "))
+
+print('you are', + (current) - (age))
